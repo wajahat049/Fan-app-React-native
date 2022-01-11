b2810f50bc6dfa0771fcc9b67c25f30b802921fa
Wajahat Ahmed <64039031+Wajahat049@users.noreply.github.com>
2022-01-11 22:11:56 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,88 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React,{useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+import Navigator from "./src/navigation"
+
+
+import { openDatabase } from 'react-native-sqlite-storage';
+
+const App=() =>{
+var db = openDatabase({ name: 'UserDatabase.db' });
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+        [],
+        function (tx, res) {
+          console.log('item:', res.rows);
+          if (res.rows.length == 0) {
+            // txn.executeSql('DROP TABLE IF EXISTS table_user', [])
+            tx.executeSql(
+              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_email VARCHAR(255), user_pass VARCHAR(255))',
+              []
+            );
+          }
+        }
+      );
+    });
+  }, []);
+
+
+
+  return (
+      
+       <Navigator/>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ src/AfterLogin.js
@@ -0,0 +1,246 @@
+import React,{ useEffect, useState } from 'react';
+import { View, Text, Button, TextInput,Image,ToastAndroid, Alert,TouchableOpacity,StyleSheet,PermissionsAndroid } from 'react-native';
+import {Signup,Home_Style} from "../../style.js"
+import { openDatabase } from 'react-native-sqlite-storage';
+import wifi from "react-native-wifi-reborn"
+import androidwifi from "react-native-android-wifi"
+import Modal from "react-native-modal";
+import { ScrollView } from 'react-native-gesture-handler';
+// import BackgroundTask from 'react-native-background-task'
+import BackgroundService from 'react-native-background-actions';
+import { jsonToCSV } from 'react-native-csv'
+// import RNFetchBlob from 'react-native-fetch-blob';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker'
+import { response } from 'express';
+
+function UserLogin(props) {
+//   const jsonData = `[
+//   {
+//       "Column 1": "1-1",
+//       "Column 2": "1-2",
+//       "Column 3": "1-3",
+//       "Column 4": "1-4"
+//   },
+//   {
+//       "Column 1": "2-1",
+//       "Column 2": "2-2",
+//       "Column 3": "2-3",
+//       "Column 4": "2-4"
+//   },
+//   {
+//       "Column 1": "3-1",
+//       "Column 2": "3-2",
+//       "Column 3": "3-3",
+//       "Column 4": "3-4"
+//   },
+//   {
+//       "Column 1": 4,
+//       "Column 2": 5,
+//       "Column 3": 6,
+//       "Column 4": 7
+//   }
+// ]`;
+
+// const results = jsonToCSV(jsonData);
+// console.log(results)
+
+const [firstname, setFirstname] = useState("");
+const [lastname, setLastname] = useState("");
+const [email, setEmail] = useState("");
+const [img, setImg] = useState("");
+
+
+  const [list, setList] = useState([]);
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [InpPass, setInpPass] = useState("");
+  const [InpSSID, setInpSSID] = useState("");
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+//   const download=()=>{
+//    const jsonData=`[
+//      {
+//        "FIRST_NAME":"${firstname}",
+//        "LAST_NAME":"${lastname}",
+//        "EMAIL":"${email}"
+//      }
+//    ]`
+// const results = jsonToCSV(jsonData);
+
+//     // write the current list of answers to a local csv file
+// const pathToWrite = `${RNFetchBlob.fs.dirs.DownloadDir}/mydata.csv`;
+// console.log('pathToWrite', pathToWrite);
+// // pathToWrite /storage/emulated/0/Download/data.csv
+// RNFetchBlob.fs
+//   .writeFile(pathToWrite, results, 'utf8')
+//   .then(() => {
+//     console.log(`wrote file ${pathToWrite}`);
+//     // wrote file /storage/emulated/0/Download/data.csv
+//   })
+//   .catch(error => console.error(error));
+//   }
+  // BackgroundTask.define(() => {
+  //   console.log('Hello from a background task')
+  //   BackgroundTask.finish()
+  // })
+useEffect(()=>{
+  // BackgroundService.start(()=>{console.log("background")},{
+  //   taskName: 'Example',
+  //   taskTitle: 'ExampleTask title',
+  //   taskDesc: 'ExampleTask description',
+  //   taskIcon: {
+  //       name: 'ic_launcher',
+  //       type: 'mipmap',
+  //   },
+  //   color: '#ff00ff',
+  //   linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+  //   parameters: {
+  //       delay: 1000,
+  //   },
+  // } );
+
+  // BackgroundService.stop()
+  // BackgroundTask.schedule({period:5})
+//   const granted =  PermissionsAndroid.request(
+//     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+//     {
+//       title: 'Location permission is required for WiFi connections',
+//       message:
+//         'This app needs location permission as this is required  ' +
+//         'to scan for wifi networks.',
+//       buttonNegative: 'DENY',
+//       buttonPositive: 'ALLOW',
+//     },
+// );
+// if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+//   // You can now use react-native-wifi-reborn
+// } else {
+//   // Permission denied
+// }
+// androidwifi.setEnabled(false)
+// androidwifi.loadWifiList((wifiStringList) => {
+//   var wifiArray = JSON.parse(wifiStringList);
+//   setList(wifiArray)
+//     // console.log("slslxsxn",wifiArray);
+//   },
+//   (error) => {
+//     console.log(error);
+//   }
+// );
+
+},[])
+
+
+//  const pressed=()=>{
+// wifi.loadWifiList().then((l)=>console.log("newlist",l))
+
+// androidwifi.loadWifiList((wifiStringList) => {
+//   var wifiArray = JSON.parse(wifiStringList);
+//     console.log("slslxsxn",wifiArray);
+//   },(error) => {
+//     console.log(error);
+//   })
+
+//  }
+  // ______WIFI______
+  const openToggle=(SSID)=>{
+    androidwifi.setEnabled(false)
+    setInpSSID(SSID)
+    toggleModal()
+
+  }
+
+  const onConnected=()=>{
+    if(InpPass!=""){
+      // console.log("inside")
+    wifi.connectToProtectedSSID(InpSSID, InpPass, true).then(
+      () => {
+        
+        console.log("Connected successfully!");
+        ToastAndroid.show("Connected",ToastAndroid.SHORT)
+        setInpPass("")
+        toggleModal()
+      },
+      () => {
+        console.log("Connection failed!");
+      }
+    );}
+    else{
+      ToastAndroid.show("Enter Password",ToastAndroid.SHORT)
+    }
+
+  }
+
+
+  const openPicker= () =>{
+
+    launchImageLibrary({mediaType:"photo",noData:true},response=>{
+      // console.log("response",res.assets[0].base64)
+      console.log("aknaxk")
+      setImg(response.assets[0])
+      console.log("aknaxk",response.assets[0].uri)
+      
+    })
+
+  }
+    return(
+      <View style={{ marginTop:"5%" }}>
+
+      <Text style={{ fontWeight: "bold", fontSize: 32 }}>All Wifi Networks</Text>
+
+
+{/* {console.log("aaaa",list)} */}
+{/* <ScrollView> */}
+      {/* {list.map((e, i) => {
+        return (
+          <TouchableOpacity onPress={()=>{openToggle(e.SSID)}} key={i} style={{backgroundColor:"#2F89FF",padding:"3%",margin:"2%"}}>
+            <Text style={{color:"white",fontSize:20}}>{e.SSID}</Text>
+          </TouchableOpacity>
+        )
+      })} */}
+      {/* <Button title="disconnect" color="red" onPress={() =>androidwifi.setEnabled(false)} />
+      <TextInput value={firstname} onChangeText={(e)=>{setFirstname(e)}} placeholder='first name'  />
+      <TextInput value={lastname} onChangeText={(e)=>{setLastname(e)}} placeholder='last name'  />
+      <TextInput value={email} onChangeText={(e)=>{setEmail(e)}} placeholder='email'  />
+
+    
+      <Button title="Download" color="yellow" onPress={() =>download()} /> */}
+
+
+      {/* </ScrollView> */}
+
+
+      <View  style={{justifyContent:"center",textAlign:"center"}}>
+
+        <Image style={{width:200,height:150,margin:10}} source={{uri:img.uri}}/>
+        <View style={{textAlign:"start",alignSelf:"center"}}>
+        <Button onPress={()=>openPicker()}  title='Upload'/>
+        </View>
+        <TextInput value={firstname} onChangeText={(e)=>{setFirstname(e)}} placeholder='first name'  />
+      <TextInput value={lastname} onChangeText={(e)=>{setLastname(e)}} placeholder='last name'  />
+      <TextInput value={email} onChangeText={(e)=>{setEmail(e)}} placeholder='email' />
+      </View>
+
+
+      {/* <View style={{ marginTop: 10 }}>
+        <Modal isVisible={isModalVisible}>
+          <View style={{ margin: 20, padding: 20, }}>
+            <TextInput keyboardType="visible-password" placeholder="Password" value={InpPass} onChangeText={(e) => setInpPass(e)} />
+            <Button title="Connect" onPress={() => onConnected()} />
+            <View style={{marginTop:20}} >
+            <Button title="Close" onPress={() => toggleModal()} />
+
+            </View>
+          </View>
+        </Modal>
+      </View> */}
+
+    </View>
+     
+    )
+
+}
+
+ 
+export default UserLogin
